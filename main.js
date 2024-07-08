2432851ad781bab53f33af4f303c85e28b6b4a75
Samartha_J <jsamarth505@gmail.com>
2024-07-09 00:33:52 +0530

    Desktop UI v1

--- main.js
+++ main.js
@@ -3,19 +3,20 @@ let long = 75.3403;
 var map = L.map('map').setView([lat, long], 13);
 // tileLayer: OSM(open street map)
 var tileLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-maxZoom: 19,
-attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 //marker
 var marker = L.marker([lat, long], {color:'red'}).addTo(map);
 
-
 document.addEventListener('DOMContentLoaded', function() {
-    const hamberguer = document.querySelector('.hamberguer');
-    const sidebar = document.querySelector('.sidebar');
+    const toggleBtn = document.querySelector('.toggle-btn');
+    const sidebar = document.getElementById('mySidebar');
+    const content = document.querySelector('.content');
 
-    hamberguer.addEventListener('click', function() {
-        sidebar.classList.toggle('open');
+    toggleBtn.addEventListener('click', function() {
+        sidebar.classList.toggle('closed');
+        content.classList.toggle('expanded');
     });
 });
